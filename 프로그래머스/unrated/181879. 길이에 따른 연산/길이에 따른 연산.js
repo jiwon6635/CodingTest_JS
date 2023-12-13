c52d58eafbf8e046dd2784d9cb2da97536a533d6
jiwon.kim <126536438+jiwon6635@users.noreply.github.com>
2023-12-13 17:20:47 +0900

    [level unrated] Title: 길이에 따른 연산, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181879. 길이에 따른 연산/길이에 따른 연산.js
@@ -0,0 +1,17 @@
+function solution(num_list) {
+    let answer;
+    
+    if (num_list.length <= 10) {
+        answer = 1;
+        for (let i=0; i < num_list.length; i++) {
+            answer *= num_list[i];
+        }
+    }
+    else {
+        answer = 0;
+        for (let i=0; i< num_list.length; i++) {
+            answer += num_list[i];
+        }
+    }
+    return answer;
+}
